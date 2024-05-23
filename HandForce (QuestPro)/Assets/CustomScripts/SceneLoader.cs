ee824d0de01dcfb3e1436958aa5d6ebb37fd07f8
NikolaKlick <n.klickova@student.fontys.nl>
2024-05-23 11:43:03 +0200

    Main menu and Exercises menu
    UI

---
+++ HandForce (QuestPro)/Assets/CustomScripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
